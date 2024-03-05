b0e8f41e580107ad454d71578e432f871baf0fe1
Samuel Torres <158462704+EMOT3T@users.noreply.github.com>
2024-03-05 09:55:27 -0300

    Update login.php
    Removal of the part related to character blocking.

--- login/php/login.php
+++ login/php/login.php
@@ -10,66 +10,45 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $email = sanitizeInput($email);
 
         $password = sanitizeInput($_POST["password"]);
-        $password = trim($password);
 
-        $blocked_characters = ["'", '"', ';', '-', '*', '/', '#', '|', '=', '$', '_', ':', '%'];
+        $sql = "SELECT * FROM users WHERE email = :email";
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+        $user = $stmt->fetch();
 
-        if (blockedChar($email, $blocked_characters)) {
-            header("Location: ../index.html?Error=Blocked_characters_detected_in_email");
-            exit;
-        } else {
-            if (blockedChar($password, $blocked_characters)) {
-                header("Location: ../index.html?Error=Blocked_characters_detected_in_password");
-                exit;
-            } else {
+        if ($user) {
+            if (!empty($user['password'])) {
+                if (password_verify($password, $user['password'])) {
+                    $_SESSION['user_id'] = $user['id'];
+                    $_SESSION['user_email'] = $user['email'];
+                    $_SESSION['user_level'] = $user['user_level'];
 
-                $sql = "SELECT * FROM users WHERE email = :email";
-                $stmt = $pdo->prepare($sql);
-                $stmt->bindParam(':email', $email);
-                $stmt->execute();
-                $user = $stmt->fetch();
-
-                if ($user) {
-                    if (!empty($user['password'])) {
-                        if (password_verify($password, $user['password'])) {
-                            $_SESSION['user_id'] = $user['id'];
-                            $_SESSION['user_email'] = $user['email'];
-                            $_SESSION['user_level'] = $user['user_level'];
-
-                            header("Location: ../../main/Pages/dashboard/index.php");
-                            exit;
-                        } else {
-                            header("Location: ../index.html?Error=Invalid_password");
-                            exit;
-                        }
-                    }
+                    header("Location: ../../main/Pages/dashboard/index.php");
+                    exit;
+                } else {
+                    header("Location: ../index.html?Error=Invalid_password");
+                    exit;
                 }
             }
+        } else {
+            header("Location: ../index.html?Error=User_not_found");
+            exit;
         }
     } else {
-        header("location: ../index.html?Error=email_or_password_is_empty!");
+        header("Location: ../index.html?Error=Email_or_password_is_empty");
         exit;
     }
 } else {
-    header("location: ../index.html?Error=Request_method_was_not_post!");
+    header("Location: ../index.html?Error=Request_method_was_not_post");
     exit;
-}
-
-function blockedChar($password, $blocked_characters)
-{
-    foreach ($blocked_characters as $char) {
-        if (strpos($password, $char) !== false) {
-            return true;
-        }
-    }
-    return false;
 }
 
 function sanitizeInput($data)
 {
     $data = trim($data);
     $data = stripslashes($data);
-    $data = htmlspecialchars($data);
+    $data = htmlspecialchars($data, ENT_QUOTES, 'UTF-8');
     return $data;
 }
 ?>
