766f0b68e14db662011944b2845c9a04d3af4f0a
Samuel Torres <158462704+EMOT3T@users.noreply.github.com>
2024-04-02 10:06:30 -0300

    Update users.sql

--- CONTENT/users.sql
+++ CONTENT/users.sql
@@ -1,6 +1,7 @@
 /* Understand that this is just a simple model. Develop and model the code according to your needs. */
 
-create table users (
+CREATE TABLE users (
+  user_id INTEGER PRIMARY KEY AUTOINCREMENT,
   user_fullname VARCHAR(502),
   user_email VARCHAR(128),
   user_password_hash VARCHAR(256),
