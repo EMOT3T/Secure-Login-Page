b7d36de7d4e50a60eb4bcbca7f60e12f2e491f22
EMOT3T <samueltorres.dev@gmail.com>
2024-04-02 12:31:38 +0000

    Added sql code

---
+++ content/users.sql
@@ -0,0 +1,8 @@
+/* Understand that this is just a simple model. Develop and model the code according to your needs. */
+
+create table users (
+  user_fullname VARCHAR(502),
+  user_email VARCHAR(128),
+  user_password_hash VARCHAR(256),
+  user_salt VARCHAR(128)
+);
